172f3f3435a9d5cb089aef0475311c29da411b7e
Ric <38759817+rictoken@users.noreply.github.com>
2024-06-01 19:10:46 +0200

    Update SLY_Assistant.user.js

--- SLY_Assistant.user.js
+++ SLY_Assistant.user.js
@@ -19,10 +19,10 @@
 	'use strict';
 
 	//Used for reading solana data
-	let customReadRPCs = [https://rpc.ironforge.network/mainnet?apiKey=01HRBY18SZS61AS2VP6CD7NXFQ];
+	let customReadRPCs = [https://rpc.ironforge.network/mainnet?apiKey=01HRBY18SZS61AS2VP6CD7NXFQ,];
 
 	//Used for pushing transactions to solana chain
-	let customWriteRPCs = [https://rpc.ironforge.network/mainnet?apiKey=01HRBY18SZS61AS2VP6CD7NXFQ];
+	let customWriteRPCs = [https://rpc.ironforge.network/mainnet?apiKey=01HRBY18SZS61AS2VP6CD7NXFQ,];
 
     let saRPCs = [
         'https://twilight-autumn-diagram.solana-mainnet.quiknode.pro/4fc53d638efd1cc0f80764bc457944bb325d1ff1', //Quicknode
